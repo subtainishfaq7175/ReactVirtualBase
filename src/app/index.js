4338acf5d556a2cd8352de9e128a29b0502cb87f
Subtain Ishfaq <s.subtain@gmail.com>
2017-09-26 22:50:36 +0500

    AddedFirstComponent

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,14 @@
-console.log("webpack==> it works");
+import React from "react";
+import {render} from "react-dom";
+
+class App extends React.Component{
+    render() {
+        return (
+            <div>
+                <h1>Hello From Virtual Base!</h1>
+            </div>
+        );
+    }
+}
+
+render(<App/>,window.document.getElementById("vbApp"));
